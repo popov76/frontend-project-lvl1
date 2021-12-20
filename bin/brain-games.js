3fa4514b39bfe4649d327b111e23e5a5445ea432
Alexey Popov <popov76@gmail.com>
2021-12-20 20:06:27 +0300

    hello git action

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 import * as cli from '../src/cli.js';
+
 console.log('Welcome to the Brain Games');
 cli.hello();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const hello = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}`);
-}
+const hello = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+};
+export default { hello };
